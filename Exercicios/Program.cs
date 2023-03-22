3528772f9c7e10e02c4d3c70877343d7b83e3ee2
Frank Alves <frank77invest@gmail.com>
2023-03-22 18:57:00 -0300

    Commit teste

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Internal;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -48,6 +49,7 @@ namespace Exercicios
 
             foreach (Employee empregado in Empregados)
             {
+                Console.WriteLine("teste");
                 Console.WriteLine(empregado);
             }
         }
