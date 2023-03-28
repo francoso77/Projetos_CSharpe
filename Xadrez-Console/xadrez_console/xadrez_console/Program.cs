76795b23085cd0519f1a3d1019c3f6da66f3edf9
francoso77 <frank77invest@gmai.com>
2023-03-28 16:56:31 -0300

    Criando posição tabuleiro

--- Xadrez-Console/xadrez_console/xadrez_console/Program.cs
+++ Xadrez-Console/xadrez_console/xadrez_console/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace xadrez_console
+﻿using tabuleiro;
+
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            posicao p = new posicao(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
---
+++ Xadrez-Console/xadrez_console/xadrez_console/tabuleiro/posicao.cs
@@ -0,0 +1,24 @@
+﻿namespace tabuleiro
+{
+    class posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public posicao(int linha, int coluna) 
+        { 
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return "Posição: ("
+                + Linha
+                + ", "
+                + Coluna
+                + ") ";
+
+        }
+    }
+}
