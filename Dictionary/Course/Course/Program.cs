f97c8459b0ce55a04ebef5d50e4f4cd4eef194fc
francoso77 <frank77invest@gmai.com>
2023-04-12 16:37:14 -0300

    Exercicio Dictionary

--- Dictionary/Course/Course/Program.cs
+++ Dictionary/Course/Course/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> cookies = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] lines = sr.ReadLine().Split(',');
+                        string name = lines[0];
+                        int votosLidos = int.Parse(lines[1]);
+                                               
+                        if (cookies.ContainsKey(name))
+                        {
+                            cookies[name] += votosLidos;
+                        }
+                        else
+                        {
+                            cookies[name] = votosLidos;
+                        }
+
+                    }
+
+                    foreach (var votos in cookies)
+                    {
+                        Console.WriteLine(votos.Key + ": " + votos.Value);
+                    }
+                    
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
